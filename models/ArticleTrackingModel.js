9a4ae69d065a404c8bb60f0aa808d18348f73493
iamraufu <eftykharraufu@gmail.com>
2024-04-24 13:23:16 +0600

    [Updated] Article Tracking Model

--- models/ArticleTrackingModel.js
+++ models/ArticleTrackingModel.js
@@ -24,7 +24,7 @@ const articleTrackingSchema = new mongoose.Schema({
       status: {
             type: String,
             default: "in document",
-            enum: ["in document", "inbound picking", "inbound picked", "inbound packing", "inbound packed", "outbound delivering", "outbound delivered", "in transit", "ready for child packing"]
+            enum: ["in document", "inbound picking", "partially inbound picked", "inbound picked", "inbound packing", "inbound packed", "outbound delivering", "outbound delivered", "in transit", "ready for child packing"]
       },
       expiryDate: {
             type: Date,
