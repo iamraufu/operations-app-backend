0d6382aea4e7ef5359d435cf5d73b95aab58a9af
iamraufu <eftykharraufu@gmail.com>
2024-06-04 12:20:01 +0600

    [Updated] User Model

--- models/UserModel.js
+++ models/UserModel.js
@@ -18,7 +18,7 @@ const userSchema = new mongoose.Schema({
       },
       staffId: {
             type: Number,
-            required: true,
+            // required: true,
             immutable: true
       },
       site: {
