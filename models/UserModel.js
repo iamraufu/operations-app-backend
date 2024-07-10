bae9223ab2c463feb572ccddc5303f685cd0a3e4
iamraufu <eftykharraufu@gmail.com>
2024-07-10 16:35:40 +0600

    [Added] User App Version

--- models/UserModel.js
+++ models/UserModel.js
@@ -16,6 +16,9 @@ const userSchema = new mongoose.Schema({
             type: String,
             required: true
       },
+      appVersion: {
+            type: String,
+      },
       staffId: {
             type: String,
             // required: true,
