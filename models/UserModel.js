c77da94569e3267c554386881a81525d4017352d
iamraufu <eftykharraufu@gmail.com>
2024-07-15 13:44:20 +0600

    [Update] UserModel email, id

--- models/UserModel.js
+++ models/UserModel.js
@@ -10,7 +10,7 @@ const userSchema = new mongoose.Schema({
             type: String,
             unique: true,
             // required: true,
-            immutable: true
+            // immutable: true
       },
       password: {
             type: String,
@@ -23,7 +23,7 @@ const userSchema = new mongoose.Schema({
             type: String,
             // required: true,
             unique: true,
-            immutable: true
+            // immutable: true
       },
       site: {
             type: Array,
